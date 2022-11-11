dd1d2f5f3be41682d50448fbd9f24157da73bc4b
namubar <mubarakshin.nail@tatar.ru>
2022-11-11 22:22:02 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿double[] array = { 3, 7, 22, 2, 78 };                      //Создание  массива, заполненного вещественными числами;
+int i;
+void fillArray (double [] array)                            //Заполнение массива
+{
+for (i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+}
+fillArray (array);
+
+Console.WriteLine();
+
+double findMax(double[] array)
+{
+    double max;                                     //Поиск максимального значения в массиве
+    max = array[0];
+    for (i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+double max = findMax(array);
+
+Console.WriteLine("Максимальное значение: " + max);
+
+double findMin(double[] array)
+{
+    double min;                                        //Поиск минимального в массиве;
+    min = array[0];
+    for (i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+double min = findMin(array);
+Console.WriteLine("Минимальное значение: " + min);
+Console.WriteLine("Разность: " + (max - min));                     //разность 
